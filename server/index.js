034e6a2f9d12cb6786f75097ff609152167b40a1
wamae-ndiritu <wamaejoseph392@gmail.com>
2025-02-23 10:46:29 +0300

    Implement authentication logic

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routes/index.js';
 
 
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+
+// routes
+app.use('/api/users', userRouter);
+// app.use('/api/products', productRouter);
+
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
 })
