f1d177079fa58f5e5c48a85e1c00f9799f45a02a
Matuwamai <wamaimatu@gmail.com>
2025-02-25 12:42:53 +0300

    Setup admin app

---
+++ admin/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='grid grid-col-2'>
+        <div className='h-screen absolute Login'>
+            <img src="" alt="" />
+        </div>
+        <div className='bg-blue-100 '>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <form action=" "></form>
+    </div>
+  )
+}
+
+export default Register
